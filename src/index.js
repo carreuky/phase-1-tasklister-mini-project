fe6c6b34119835ba77b8687de95897ddf17e1630
carreuky <carreuky@gmail.com>
2022-06-13 22:46:36 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    // console.log(e.target['new-task-description'].value)
+    p = document.createElement('p');
+    p.textContent = `${e.target['new-task-description'].value} `
+    button = document.createElement('button')
+    button.textContent = 'X'
+    console.log(p)
+    form.appendChild(p)
+    p.appendChild(button)
+
+    // ON CLICK DELETES 
+    button.addEventListener('click', function(e){
+      e.target.parentNode.remove()
+    })
+   
+  })
+  
+  
+  
+
 });
+
+
